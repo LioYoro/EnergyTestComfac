91fa3bad9155fa823a0920301feb8fe1edf47ff3
LioYoro <yoroleo10.com>
2026-01-07 11:37:50 +0800

    Add backend and frontend changes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EnergyDataController;
 
 Route::get('/energy/summary', [EnergyDataController::class, 'summary']);
+Route::get('/test', fn() => response()->json(['message' => 'API works!']));
